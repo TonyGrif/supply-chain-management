e8e8818e2d16db6224ab05f36b0fc0421d220ff9
TonyGrif <TonyGriffin2000@gmail.com>
2022-11-03 19:08:08 -0400

    loop set up

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
-using namespace std;
+#include "../headers/Factory.h"
+#include "../headers/Supplier.h"
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	Factory factory;
+	Supplier supplierA('A'), supplierB('B'), supplierC('C');
+	char cont;
+
+	factory.AddNewPartType(2);
+	factory.AddNewPartType(3);
+
+	do
+	{
+		std::cout << "Press Enter to Continue: ";
+		std::cin.get(cont);
+	} while (cont == '\n');
+
 	return 0;
 }
